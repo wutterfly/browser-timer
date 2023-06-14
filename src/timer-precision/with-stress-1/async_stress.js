d92ea205f2b6fc3f1049ddc507eb7c0ba433b24a
wolfsack <sebastian.koch.bonn@gmail.com>
2023-06-14 17:01:14 +0200

    .

---
+++ src/timer-precision/with-stress-1/async_stress.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", () => {
+
+
+    generate_stress().then(() => { })
+})
+
+async function generate_stress() {
+    while (true) {
+        // generate array with promises
+        let promises = []
+
+        for (i = 0; i < 5; i++) {
+           
+                promises.push(
+                    // every 100 milliseconds, try to fetch
+                    resolveTimer(100).then(() => {
+                        fetch("/timer-precision/with-stress-1/same_origin.html")
+                    })
+                )
+         
+        }
+
+        //await fetch("/timer-precision");
+        // await all promises
+        await Promise.allSettled(promises);
+    }
+}
+
+function resolveTimer(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
